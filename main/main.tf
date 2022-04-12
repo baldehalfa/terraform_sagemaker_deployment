0da909172b9c5ac09a31aee442ad053d13f15cf4
baldehalfa <baldehalfa@yahoo.fr>
2022-04-12 14:51:39 +0800

    Updating the code with new configurations

--- main/main.tf
+++ main/main.tf
@@ -1,21 +1,21 @@
 provider "aws" {
-  region = var.aws_region
+  region  = var.aws_region
   profile = var.aws_profile
   version = "4.6.0"
 }
 
 module "iam" {
-  source = "../modules/iam"
+  source     = "../modules/iam"
   aws_region = var.aws_region
 
-  iam_name = var.iam_name
+  iam_name   = var.iam_name
   identifier = var.identifier
 }
 
 module "s3" {
   source = "../modules/s3"
 
-  notebook_bucket_name = var.notebook_bucket_name
+  notebook_bucket_name  = var.notebook_bucket_name
   sagemaker_bucket_name = var.sagemaker_bucket_name
 }
 
@@ -23,7 +23,7 @@ module "sagemaker" {
   source = "../modules/sagemaker"
 
   sagemaker_notebook_name = var.sagemaker_notebook_name
-  aws_iam_role = "${module.iam.iam_role_arn}"
-  bucket_name = "${module.s3.bucket_name}"
+  aws_iam_role            = module.iam.iam_role_arn
+  bucket_name             = module.s3.bucket_name
 }
 
