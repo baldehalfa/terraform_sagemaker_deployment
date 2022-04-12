afc9aab16744842fba7fbf55d92f8d9560de9ecf
iPrice Group <baldehalfa@yahoo.fr>
2022-04-12 11:07:12 +0800

    Updating the code with new configurations

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,11 +1,11 @@
 resource "aws_s3_bucket" "notebook" {
   bucket = var.notebook_bucket_name
   force_destroy = true
-  acl = "private"
+  # acl = "private"
 }
 
 resource "aws_s3_bucket" "sagemaker" {
   bucket = var.sagemaker_bucket_name
   force_destroy = true
-  acl = "private"
+  # acl = "private"
 }
--- modules/sagemaker/main.tf
+++ modules/sagemaker/main.tf
@@ -2,10 +2,10 @@ resource "aws_sagemaker_notebook_instance" "default" {
   name = var.sagemaker_notebook_name
   role_arn = var.aws_iam_role
   instance_type = "ml.t2.medium"
-  lifecycle_config_name = aws_sagemaker_notebook_instance_lifecycle_configuration.default.name
+  # lifecycle_config_name = aws_sagemaker_notebook_instance_lifecycle_configuration.default.name
 }
 
-resource "aws_sagemaker_notebook_instance_lifecycle_configuration" "default" {
-  name = var.sagemaker_notebook_name
-  on_start = "${base64encode(data.template_file.instance_init.rendered)}"
-}
+# resource "aws_sagemaker_notebook_instance_lifecycle_configuration" "default" {
+#   name = var.sagemaker_notebook_name
+#   on_start = "${base64encode(data.template_file.instance_init.rendered)}"
+# }
